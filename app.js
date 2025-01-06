99065aeaf0751c9645ad0eb4166904ce685bdd0f
Jethanandj52 <134337163+Jethanandj52@users.noreply.github.com>
2025-01-06 17:07:25 +0500

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ var city = document.getElementById("inp").value;
 var card = document.getElementById("cards");
 let er = document.getElementById("errors");
 
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=52cad508bde81442eebec1e7b0e5045d&units=metric`)
+fetch(`https://api.openweathermap.org/data/2.5/weather?q={city}&appid=52cad508bde81442eebec1e7b0e5045d&units=metric`)
     .then((res) => {
         return res.json();
     })
